1c4356432ae7ec7936f956a76fcddb2dd938c773
Matt Miller <mattmiller899@email.arizona.edu>
2018-04-21 08:16:17 -0700

    Forgot to separate python SCRIPT

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -247,7 +247,7 @@ class Pipeline:
                     log.info('Splitting libraries of "%s" and "%s" with "%s"', forward_fastq_fp, reverse_fastq_fp, barcodes_fp)
                     #TODO Make argument for -q and --max_barcode_errors and rev_comp (1-step vs 2-step PCR?)?
                     run_cmd([
-                            'python /miniconda/bin/split_libraries_fastq.py',
+                            'python', '/miniconda/bin/split_libraries_fastq.py',
                             '-o', str(output_dir),
                             '-b', str(barcodes_fp) + ',' + str(barcodes_fp),
                             '-i', str(forward_fastq_fp) + ',' + str(reverse_fastq_fp),
@@ -279,7 +279,7 @@ class Pipeline:
                         log.info('Splitting libraries of "%s" with "%s"', input_file, barcodes_fp)
                         # TODO Make argument for -q and --max_barcode_errors and rev_comp (1-step vs 2-step PCR?)?
                         run_cmd([
-                            'python /miniconda/bin/split_libraries_fastq.py',
+                            'python', '/miniconda/bin/split_libraries_fastq.py',
                             '-o', str(output_dir),
                             '-b', str(barcodes_fp),
                             '-i', str(input_file),
@@ -319,7 +319,7 @@ class Pipeline:
                 split_fastq_basename = os.path.basename(split_fastq_fp)
                 file_name = re.split('_seqs', split_fastq_basename)[0]
                 run_cmd([
-                        'python /miniconda/bin/split_sequence_file_on_sample_ids.py',
+                        'python', '/miniconda/bin/split_sequence_file_on_sample_ids.py',
                         '-i', split_fastq_fp,
                         '-o', str(output_dir),
                         '--file_type', 'fastq'
